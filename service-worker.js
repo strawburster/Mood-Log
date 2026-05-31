2c1db63fb6a8cb245c3cb0625e18e7a87e40541f
Amy Pillow <amypillow@lavache.com>
2026-05-31 11:08:40 +0200

    Service worker cache updates
    Adding logging messages, new assets.
    
    Deletes old caches on activation if they exist.
    
    Caches new fetch response in the case of a cache miss

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,52 @@
-const CACHE_NAME = 'mood-logger-cache-v1';
+const CACHE_NAME = 'mood-logger-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  // Add your CSS, JS, images, etc. here
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/index.js',
+  '/main.css',
 ];
 
-self.addEventListener('install', event => {
+self.oninstall = event => {
+  console.log('Install event: %s', CACHE_NAME);
+  // Activate immediately after updating
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-});
+      .then(() => console.log('Cached all assets.')));
+};
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.onactivate = event => {
+  console.log('Activate event: %s', CACHE_NAME);
+  event.waitUntil(caches.keys().then(
+    cacheKeys => Promise.all(
+      cacheKeys.filter(cacheName => cacheName !== CACHE_NAME)
+        .map(oldCache => caches.delete(oldCache).then(
+          () => console.log('Deleting old cache: %s', oldCache))))));
+};
+
+self.onfetch = event => {
+  const url = new URL(event.request.url).pathname;
+  console.log('Fetch event: %s', url);
+  if (urlsToCache.includes(url)) {
+    event.respondWith(
+      caches.open(CACHE_NAME)
+        .then(cache => cache.match(event.request))
+        .then(cacheResponse => {
+          if (cacheResponse) {
+            console.log('Cache hit!');
+            return cacheResponse;
+          }
+          console.log('Cache miss!');
+          return fetch(event.request).then(
+            fetchResponse => {
+              console.log('Caching new response.');
+              cache.put(event.request, fetchResponse);
+              return fetchResponse;
+            });
+        }));
+  }
+};
